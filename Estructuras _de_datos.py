07b6df3df9ced91ba2e36877d327fc34dd5867a1
alexs <alexsquiroz054@gmail.com>
2022-10-12 19:34:44 -0500

    parte 2

--- Estructuras _de_datos.py
+++ Estructuras _de_datos.py
@@ -60,10 +60,46 @@ queue.popleft()
 queue.popleft()
 
 '''Comprension de listas'''
-
-squares = []
-
-for x in range(10):
-    squares.append(x**2)
+squares = [(x*5)/2 for x in range(10)]
 print(squares)
+
+'''Listas por compresion anidadas'''
+
+primos = [
+    [2, 3, 5, 7],
+    [11, 13, 17, 19],
+    [23, 29, 31, 37],
+]
+
+[[row[i] for row in primos] for i in range(3)]
+
+'''La instruccion del'''
+
+Rectas = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+del Rectas[0:5]
+print(Rectas)
+
+'''Tuplas y secuencias'''
+
+presentacion = 'Hola', 'me', 'llamo', 'Alexs ', ',tengo', 18, 'años.'
+nuevo = presentacion, ('un placer')
+print(nuevo)
+
+'''Conjuntos'''
+
+canasta_familiar = {'leche', 'huevos', 'pan', 'queso', 'fruta y verduras', 'carnes', 'cereales'}
+'fritos' in canasta_familiar
+'huevos' in canasta_familiar
+
+'''Diccionarios'''
+
+ventas = {'carlos': 300, 'jessica': 600, 'alfredo': 100}
+ventas['pedro'] = 200
+del ventas['alfredo']
+list(ventas)
+print(ventas)
+'carlos' in ventas
+'alfredo' in ventas
+
+'''Tecnicas de iteración'''
 
